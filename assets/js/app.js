85b8c830d116b52b99fde5b3254d0369bd22be47
JoshYewUhh <jckessell@gmail.com>
2018-10-03 11:56:47 -0500

    added maps functions

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,205 @@
- JoshuaMapsFunctions
 const key = "200367477-2b5b5ee846692e48eb30894d0d0c74ce";
 let max = "30";
-
+let pos = {lat: 44.427963, lng: -110.588455}
 
 // Initialize Map
 function initMap() {
-    let pos = {lat: 44.427963, lng: -110.588455}
-    map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 9,
-        center: pos
-    });
-   
+    var styledMapType = new google.maps.StyledMapType(
+        [
+            {
+              "elementType": "geometry",
+              "stylers": [
+                {
+                  "color": "#94e4d1"
+                }
+              ]
+            },
+            {
+              "elementType": "labels.text.fill",
+              "stylers": [
+                {
+                  "color": "#523735"
+                }
+              ]
+            },
+            {
+              "elementType": "labels.text.stroke",
+              "stylers": [
+                {
+                  "color": "#f5f1e6"
+                }
+              ]
+            },
+            {
+              "featureType": "administrative",
+              "elementType": "geometry.stroke",
+              "stylers": [
+                {
+                  "color": "#c9b2a6"
+                }
+              ]
+            },
+            {
+              "featureType": "administrative.land_parcel",
+              "elementType": "geometry.stroke",
+              "stylers": [
+                {
+                  "visibility": "off"
+                }
+              ]
+            },
+            {
+              "featureType": "administrative.land_parcel",
+              "elementType": "labels.text.fill",
+              "stylers": [
+                {
+                  "color": "#ae9e90"
+                },
+                {
+                  "visibility": "off"
+                }
+              ]
+            },
+            {
+              "featureType": "landscape.natural",
+              "elementType": "geometry",
+              "stylers": [
+                {
+                  "color": "#94e4d1"
+                }
+              ]
+            },
+            {
+              "featureType": "poi.park",
+              "elementType": "geometry.fill",
+              "stylers": [
+                {
+                  "color": "#0c8e56"
+                }
+              ]
+            },
+            {
+              "featureType": "poi.park",
+              "elementType": "labels.text.fill",
+              "stylers": [
+                {
+                  "color": "#447530"
+                }
+              ]
+            },
+            {
+              "featureType": "road",
+              "elementType": "geometry",
+              "stylers": [
+                {
+                  "color": "#f5f1e6"
+                }
+              ]
+            },
+            {
+              "featureType": "road.arterial",
+              "elementType": "geometry",
+              "stylers": [
+                {
+                  "visibility": "simplified"
+                },
+                {
+                  "weight": 1
+                }
+              ]
+            },
+            {
+              "featureType": "road.highway",
+              "elementType": "geometry",
+              "stylers": [
+                {
+                  "color": "#efefef"
+                },
+                {
+                  "weight": 0.5
+                }
+              ]
+            },
+            {
+              "featureType": "road.highway",
+              "elementType": "geometry.stroke",
+              "stylers": [
+                {
+                  "color": "#efefef"
+                }
+              ]
+            },
+            {
+              "featureType": "road.local",
+              "elementType": "geometry.fill",
+              "stylers": [
+                {
+                  "color": "#f4dffb"
+                }
+              ]
+            },
+            {
+              "featureType": "transit.line",
+              "elementType": "geometry",
+              "stylers": [
+                {
+                  "color": "#dfd2ae"
+                }
+              ]
+            },
+            {
+              "featureType": "transit.line",
+              "elementType": "labels.text.fill",
+              "stylers": [
+                {
+                  "color": "#8f7d77"
+                }
+              ]
+            },
+            {
+              "featureType": "transit.line",
+              "elementType": "labels.text.stroke",
+              "stylers": [
+                {
+                  "color": "#ebe3cd"
+                }
+              ]
+            },
+            {
+              "featureType": "transit.station",
+              "elementType": "geometry",
+              "stylers": [
+                {
+                  "color": "#dfd2ae"
+                }
+              ]
+            },
+            {
+              "featureType": "water",
+              "elementType": "geometry.fill",
+              "stylers": [
+                {
+                  "color": "#3d9cb8"
+                }
+              ]
+            },
+            {
+              "featureType": "water",
+              "elementType": "labels.text.fill",
+              "stylers": [
+                {
+                  "color": "#92998d"
+                }
+              ]
+            }
+          ],
+        {name: 'Styled Map'});
 
-    // Create infoWindow overlay
-    infoWindow = new google.maps.InfoWindow;
     
     // Try HTML5 geolocation.
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
-        var pos = {
+        pos = {
             lat: position.coords.latitude,
             lng: position.coords.longitude
             };
@@ -34,21 +215,41 @@ function initMap() {
 
         
     } else {
-
-    // Browser doesn't support Geolocation
-    handleLocationError(false, infoWindow, map.getCenter());
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
     }     
 
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 9,
+        center: pos,
+        mapTypeControl: false,
+        fullscreenControl: false,
+        streetViewControl: false
+    });
 
+    map.mapTypes.set('styled_map', styledMapType);
+    map.setMapTypeId('styled_map');
 
-console.log(pos);
-let queryURL = `https://www.hikingproject.com/data/get-trails?lat=${pos.lat}&lon=${pos.lng}&maxDistance=${max}&key=${key}`;
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(res){
-    console.log(res);
-});
+    // Create infoWindow overlay
+    infoWindow = new google.maps.InfoWindow;
+
+    $('#returnloc').on('click',function(){
+        map.setCenter(pos);
+    });
+
+    $('#populate').on('click',function(){
+        let queryURL = `https://www.hikingproject.com/data/get-trails?lat=${pos.lat}&lon=${pos.lng}&maxDistance=${max}&key=${key}`;
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(res){
+            for (let i=0; i<res.trails.length; i++) {
+                let pos = ({lat: res.trails[i].latitude, lng: res.trails[i].longitude});
+                let marker = new google.maps.Marker({position: pos, map: map});
+                marker.setMap(map);
+            }
+        });
+    });
 }
 
 
@@ -91,7 +292,9 @@ $('#cancel').on('click', function(){
         
     });
     $('#collapseOne').collapse('hide');
-})
+});
+
+
 
 
 

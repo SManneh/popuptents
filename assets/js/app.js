78c8371852ee2b5765c8878f07a198d89000cf24
Nabou <Nabou@Sainabou.local>
2018-10-03 13:38:51 -0500

    second commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,22 +2,22 @@
   
 //   event.preventDefault();
 //   const stock = $(this).val();
-navigator.geolocation.getCurrentPosition(showPosition);
+// navigator.geolocation.getCurrentPosition(showPosition);
 
-function showPosition(position){
-    console.log("Latitude: " + position.coords.latitude);  
-    console.log("Longitude: " + position.coords.longitude); 
-}
+// function showPosition(position){
+//     console.log("Latitude: " + position.coords.latitude);  
+//     console.log("Longitude: " + position.coords.longitude); 
+// }
 
 const campSites = [];
-  const queryURL = `https://developer.nps.gov/api/v1/campgrounds?stateCode=tx&api_key=8y6XS6YXPcnGb4WJty65Kktjn72zJhZ4q4jkfzkz`;
-// console.log(stock);
+const queryURL = 'https://developer.nps.gov/api/v1/campgrounds?total=611&api_key=8y6XS6YXPcnGb4WJty65Kktjn72zJhZ4q4jkfzkz';
+  
 console.log(queryURL);
 $.ajax({
   url:queryURL,
   method:"GET" 
 }).then(function(response){
-
+console.log(response);
  
 console.log(response.data[0]);
 for (let i = 0; i < response.data.length; i++){
@@ -25,23 +25,38 @@ for (let i = 0; i < response.data.length; i++){
     if ( currentItem.latLong ){
         let thisCampSite = {
             name : currentItem.name,
+            description: currentItem.description,
             location:  currentItem.latLong,
+            addresses: {
+                line: currentItem.addresses[1].line1,
+                city: currentItem.addresses[1].city,
+                state: currentItem.addresses[1].stateCode,
+                zip: currentItem.addresses[1].postalCode
+            },
             amenities: {
                 showers: currentItem.amenities.showers,
                 toilets:  currentItem.amenities.toilets,
-                internet: currentItem.amenities.internetConnectivity,
+                internetNetwork: currentItem.amenities.internetConnectivity,
                 laundry: currentItem.amenities.laundry,
                 cellPhone: currentItem.amenities.cellPhoneReception
             },
-            description: currentItem.description
+           accessibility: {
+               roads: currentItem.accessibility.accessRoads,
+               cell: currentItem.accessibility.cellPhoneInfo,
+               internet: currentItem.accessibility.internetinfo,
+               wheelChair: currentItem.accessibility.wheelChairAccess,
+               ada: currentItem.accessibility.adaInfo,
+               addInfo: currentItem.accessibility.additionalInfo
+
+           },
         };
         
         campSites.push(thisCampSite);
     }
     
-    // let contact = response.data[i].
+//     // let contact = response.data[i].
 
 }
 
-})
+});
 // });
